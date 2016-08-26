6cf27b85b43649b693ea6a329e3cf6fd3eca9a33
Elijah Ginter <ginterelijah@gmail.com>
2016-08-26 15:33:35 -0600

    Changed servlet

--- src/week6/PresidentServlet.java
+++ src/week6/PresidentServlet.java
@@ -12,37 +12,45 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/PresidentServlet")
 public class PresidentServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public PresidentServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-    
-  
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int presidentNumber = Integer.parseInt(request.getParameter("presidentNumber"));
+
+	public PresidentServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int presidentNumber;
+		if (request.getParameter("presidentNumber") == null) {
+			presidentNumber = Integer.parseInt(request.getParameter("presidentNumber"));
+		} else {
+			presidentNumber = 1;
+		}
 		request.setAttribute("presidentNumber", presidentNumber);
-		Map<Integer,President> presidents = new PresidentDAO(getServletContext()).getPresidents();
+		Map<Integer, President> presidents = new PresidentDAO(getServletContext()).getPresidents();
 		President selectedPresident = presidents.get(presidentNumber);
 		System.out.println(selectedPresident);
 		request.setAttribute("selectedPresident", selectedPresident);
 		getServletContext().getRequestDispatcher("/results.jsp").forward(request, response);
-		
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		// response.getWriter().append("Served at:
+		// ").append(request.getContextPath());
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		HttpSession session = request.getSession();
-//		if (session.getAttribute("presidents") == null){
-//			// first time make president map
-//			session.setAttribute("presidents" , new PresidentDAO().getPresidents());
-//			
-//		}
-//		
-//		request.getRequestDispatcher("/results.jsp").forward(request, response);
-//		
-//		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// HttpSession session = request.getSession();
+		// if (session.getAttribute("presidents") == null){
+		// // first time make president map
+		// session.setAttribute("presidents" , new
+		// PresidentDAO().getPresidents());
+		//
+		// }
+		//
+		// request.getRequestDispatcher("/results.jsp").forward(request,
+		// response);
+		//
+		doGet(request, response);
 	}
 
 }
